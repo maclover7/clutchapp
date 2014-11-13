ee55c54cf4101fe2d30671936778b85ab09b3424
Jon Moss <me@jonathanmoss.me>
2014-11-13 17:23:08 -0500

    Generated user model

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,6 @@
 class SessionsController < ApplicationController
   def create
-   raise env['omniauth.auth'].to_yaml
+   # raise env['omniauth.auth'].to_yaml
+   user = User.from_omniauth(env['omniauth.auth'])
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,21 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113204326) do
+ActiveRecord::Schema.define(version: 20141113222222) do
 
   create_table "assignments", force: true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
     t.boolean  "work_allowed", default: false
+  end
+
+  create_table "users", force: true do |t|
+    t.string   "provider"
+    t.string   "uid"
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
