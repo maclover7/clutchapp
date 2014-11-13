675132801c173d009146d54d83d9820c4e138b35
Jon Moss <me@jonathanmoss.me>
2014-11-13 18:29:38 -0500

    Users can create assignments!

--- app/controllers/assignments_controller.rb
+++ app/controllers/assignments_controller.rb
@@ -69,6 +69,6 @@ class AssignmentsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def assignment_params
-      params.require(:assignment).permit(:name)
+      params.require(:assignment).permit(:name, :work_allowed, :user_id)
     end
 end
