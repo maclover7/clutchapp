821e1ea1dcef4c9ba0d8ea6545aed246f07b069a
Jon Moss <me@jonathanmoss.me>
2014-11-13 18:31:02 -0500

    Added admin boolean to users

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141113231903) do
+ActiveRecord::Schema.define(version: 20141113233017) do
 
   create_table "assignments", force: true do |t|
     t.string   "name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20141113231903) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "email"
+    t.boolean  "admin",      default: false
   end
 
 end
