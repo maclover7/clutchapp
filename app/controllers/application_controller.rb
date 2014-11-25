8f475421e307597af7283e7de958570817d52408
Jon Moss <me@jonathanmoss.me>
2014-11-25 16:09:55 -0500

    working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -11,11 +11,10 @@ class ApplicationController < ActionController::Base
     end
 
     def login_required
-      if session[:user]
+      if session[:user_id]
         return true
       end
-      flash[:warning]='Please login to continue'
-      redirect_to "/auth/google_oauth2"
+        redirect_to "/auth/google_oauth2"
       return false
     end
 end
