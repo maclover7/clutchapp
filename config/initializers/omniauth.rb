fc59ff97a85bc215d4c65877049fcb3accd12bb7
Jon Moss <me@jonathanmoss.me>
2014-11-29 10:30:15 -0500

    trying to set hd by env variables...

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"], {
-    :hd => 'jonathanmoss.me'
+    :hd => ENV["domain"]
   }
 
   OmniAuth.config.logger = Rails.logger
