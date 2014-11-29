dc6bbc0212fd58e31cfd5c290c95a4f6ef9bfbe3
Jon Moss <me@jonathanmoss.me>
2014-11-29 10:26:30 -0500

    three times is a charm

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,6 +1,6 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
   provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"], {
-    :hd => 'gmail.com'
+    :hd => 'jonathanmoss.me'
   }
 
   OmniAuth.config.logger = Rails.logger
