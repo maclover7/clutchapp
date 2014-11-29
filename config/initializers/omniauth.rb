319925e6267aa400f511d81c228b82a575976392
Jon Moss <me@jonathanmoss.me>
2014-11-29 10:17:48 -0500

    custom domain with google

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,4 +1,7 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"]
+  provider :google_oauth2, ENV["google_client_id"], ENV["google_client_secret"], {
+    :hd => ENV['domain']
+  }
+
   OmniAuth.config.logger = Rails.logger
 end
